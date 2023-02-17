643adaa2ad9508260cc4011eeb055510643ceadc
Suitret <suitret01@gmail.com>
2023-02-17 05:23:06 -0800

    reverse alphabet letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
