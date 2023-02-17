4922afc576eafbcde5ed28ea22a4c0c90c13f1d4
Suitret <suitret01@gmail.com>
2023-02-17 05:06:43 -0800

    alphabetic letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
