3e5a8011e6226c6c0e94ac4139854ed3cdfa294a
Suitret <suitret01@gmail.com>
2023-02-17 07:33:46 -0800

    combinaison ab cd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		for (j = 48; (j <= i) && (j < 57); j++)
+		for (j = i; (j < 57); j++)
 		{
 			for (k = i; (k < 58); k++)
 			{
-				for (l = j + 1; (l > j) && (l < 58); l++)
+				for (l = j + 1; (l < 58); l++)
 				{
 					putchar(i);
 					putchar(j);
