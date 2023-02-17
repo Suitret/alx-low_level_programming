f7237b362196ad8fd61dbcb192e6031baa822707
Suitret <suitret01@gmail.com>
2023-02-17 03:01:59 -0800

    show lowercase letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+		putchar(10);
+	}
+	return (0);
+}
