a73f2ec2213b3989fb36bd35e30159f66d48a43d
Suitret <suitret01@gmail.com>
2023-02-17 05:20:02 -0800

    digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
