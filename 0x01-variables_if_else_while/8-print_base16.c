9dea305198b47ce2042758cd35ee247cdf641272
Suitret <suitret01@gmail.com>
2023-02-17 05:28:29 -0800

    base 16 digit numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
