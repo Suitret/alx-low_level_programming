18b495760481ac8896fb5f3723f22150e8277f59
Suitret <suitret01@gmail.com>
2023-02-21 15:00:58 -0800

    Print 100 first fibo values

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,24 +9,15 @@
 
 int main(void)
 {
-	long int a = 1, b = 2, n = 99, temp;
+	unsigned long int a = 1, b = 2, n = 99, temp;
 
-	printf("%ld, ", a);
+	printf("%lu, %lu", a, b);
 	while (n != 0)
 	{
-		if (n != 1)
-		{
-			printf("%ld, ", b);
-		}
-		else
-		{
-			printf("%ld", b);
-		}
 		temp = b;
 		b += a;
 		a = temp;
-		if (b < 0)
-			b = -b;
+		printf(", %lu", b);
 		n--;
 	}
 	printf("\n");
