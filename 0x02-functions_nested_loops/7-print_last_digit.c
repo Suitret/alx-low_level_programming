cced162e78ead1c98a10b3d922982743081efbe8
Suitret <suitret01@gmail.com>
2023-02-21 09:53:45 -0800

    give the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - give the last digit of a given number
+ * @n: given number
+ * Return: the last digit of the given number
+ */
+int print_last_digit(int n)
+{
+	int temp = n % 10;
+
+	_putchar(temp + '0');
+	return (temp);
+}
