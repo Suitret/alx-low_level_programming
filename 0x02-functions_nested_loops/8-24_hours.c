5ad8fcf3ac0e24d69b91ea71f1ea02e9586946bc
Suitret <suitret01@gmail.com>
2023-02-21 11:33:06 -0800

    24 hour chrono

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - print hour each minute
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; ((i < 2) && (j < 10)) || (j < 4); j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+				}
+			}
+		}
+	}
+}
