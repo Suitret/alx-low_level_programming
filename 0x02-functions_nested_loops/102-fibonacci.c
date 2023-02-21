499c23ec0c6cd8c4aff845e76652314cbf7b9016
Suitret <suitret01@gmail.com>
2023-02-21 13:12:12 -0800

    term of Fibonaci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 1, b = 2, n = 47, temp;
+
+	printf("%d, ", a);
+	while (n != 0)
+	{
+		if (n != 1)
+		{
+			printf("%d, ", b);
+		}
+		else
+		{
+			printf("%d", b);
+		}
+		temp = b;
+		b += a;
+		a = temp;
+		n--;
+	}
+
+	return (0);
+}
