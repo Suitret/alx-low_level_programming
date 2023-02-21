d4541dc3d51ec4d281326b97ba32236eb27f6e5c
Suitret <suitret01@gmail.com>
2023-02-21 12:25:47 -0800

    increase or decrease to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers from n to 98
+ * @n: given number
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n != 99)
+		{
+			printf("%d", n);
+			if (n <= 97)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n != 97)
+		{
+			printf("%d", n);
+			if (n >= 99)
+				printf(", ");
+			n--;
+		}
+	}
+}
