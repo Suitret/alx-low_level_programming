fa67dff643270a72ccbbabc180b59818dff8fc35
Suitret <suitret01@gmail.com>
2023-02-21 09:22:53 -0800

    print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - print alphabet in lowercase
  *
  * Return: void
