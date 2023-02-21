437276e22cd6c139bb2bb79ad8903c032f827c57
Suitret <suitret01@gmail.com>
2023-02-21 14:02:06 -0800

    first 100 term of fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int a = 1, b = 2, n = 99, temp;
+
+	printf("%ld, ", a);
+	while (n != 0)
+	{
+		if (n != 1)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld", b);
+		}
+		temp = b;
+		b += a;
+		a = temp;
+		n--;
+	}
+	printf("\n");
+	return (0);
+}
