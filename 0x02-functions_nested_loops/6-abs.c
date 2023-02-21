c974035e16e60ba629be59c6e9ab9873d4f15973
Suitret <suitret01@gmail.com>
2023-02-21 09:51:09 -0800

    give the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - give the absolute value of a number
+ * @n: given number
+ * Return: Always the positive value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
