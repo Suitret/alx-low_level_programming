281519b5803d059a31f8fb9e97eecd744b6f2888
Suitret <suitret01@gmail.com>
2023-02-21 09:37:11 -0800

    Check if a character is alphabetic or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,5 @@
-#include <stdio.h>
+#include <ctype.h>
 #include "main.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	int r
-	r = _isalpha('H');
-	_putchar(r +'0');
-	r = _isalpha('o');
-	_putchar(r +'0');
-	r = _isalpha(108);
-	_putchar(r +'0');
-	_putchar('\n');
-	return (0);
-}
 
 /**
  * _isalpha - check for alphabetic character
