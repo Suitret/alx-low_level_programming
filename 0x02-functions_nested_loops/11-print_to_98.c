702e589702e3b3e6883bc985f9bfd4564cef5485
Suitret <suitret01@gmail.com>
2023-02-21 12:31:47 -0800

    increase or decrease to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,8 @@ void print_to_98(int n)
 		while (n != 99)
 		{
 			printf("%d", n);
+			if (n == 98)
+				printf(" ");
 			if (n <= 97)
 				printf(", ");
 			n++;
@@ -24,6 +26,8 @@ void print_to_98(int n)
 		while (n != 97)
 		{
 			printf("%d", n);
+			if (n == 98)
+				printf(" ");
 			if (n >= 99)
 				printf(", ");
 			n--;
