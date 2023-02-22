e914342d33469e6b1cadf887a133d8cc72995204
Suitret <suitret01@gmail.com>
2023-02-22 11:52:36 -0800

    comments can sometimes fix problems

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
@@ -12,12 +11,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
-*while (i < 10)
-*{
-*	putchar(i);
-*}
-*/
+	/**
+	* while (i < 10)
+	* {
+	*	putchar(i);
+	* }
+	*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
