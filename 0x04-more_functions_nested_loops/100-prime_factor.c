72851c5eab7d728c5ad904c5777604f3f7043bf3
Suitret <suitret01@gmail.com>
2023-02-23 09:47:02 -0800

    print the largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * isPrime - check if a number is prime or not
+ * @n: number to check
+ * Return: 0 or 1
+ */
+
+int isPrime(int n)
+{
+	int i, temp = 0;
+
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			temp = 1;
+			break;
+		}
+	}
+	if (temp == 0)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int n = 612852475143, prime;
+	double temp = sqrt(n);
+	int i;
+
+	for (i = 2; i < temp; i++)
+	{
+		if (isPrime(i) && (n % i == 0))
+		{
+			n /= i;
+			prime = i;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
