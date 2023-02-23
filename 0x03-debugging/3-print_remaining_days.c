6ab53dd15f97dfe05d0a5c1877d47e5eca67b258
Suitret <suitret01@gmail.com>
2023-02-22 19:28:51 -0800

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,7 +26,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{		
-			printf("Day of the year: %d\n", 31);
+			printf("Day of the year: %d\n", 60);
 			printf("Remaining days: %d\n", 306);
 		}
 	}
