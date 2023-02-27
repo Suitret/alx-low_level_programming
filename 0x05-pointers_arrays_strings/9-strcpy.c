6daf53b08dd2147e97d1fd2a2120c376c0079bcb
Suitret <suitret01@gmail.com>
2023-02-27 13:55:20 -0800

    copy of string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - copy of string
+ * @dest: pointer on char
+ * @src: pointer on char
+ * Return: pointer on char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
+
