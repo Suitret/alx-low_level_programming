54b6d641d64182d66c245c851d8d85f8c6a21fef
Suitret <suitret01@gmail.com>
2023-02-27 13:13:49 -0800

    prints character at even position

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints character at even position
+ * @str: pointer on char
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0, n = strlen(s);
+
+	while (i < n)
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
+
