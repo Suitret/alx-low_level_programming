0b35763d532ec9d5e939ac2e58e9aca476ffe269
Suitret <suitret01@gmail.com>
2023-02-27 11:39:56 -0800

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer on char
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	char *ptr = s;
+	int i = strlen(s);
+
+	while (i != -1)
+	{
+		_putchar(*(ptr + i));
+		i--;
+	}
+}
