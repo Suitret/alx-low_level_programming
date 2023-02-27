7977627511c82c8376773698e46ab1540205333c
Suitret <suitret01@gmail.com>
2023-02-27 14:04:53 -0800

    converts string into integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - converts string into integer
+ * @s: pointer on char
+ * Return: integer obtained
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
