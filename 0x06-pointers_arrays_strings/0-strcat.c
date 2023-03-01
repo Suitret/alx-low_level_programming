d3b8588219c656b91496fe984f5e3bc78a3b0a70
Suitret <suitret01@gmail.com>
2023-02-28 23:19:40 -0800

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: result of concatenation
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
