17918906d4b089ce16c3ae9805bb8a21d01e8bf3
Suitret <suitret01@gmail.com>
2023-03-01 13:28:44 -0800

    prints any integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints any integer
+ * @n: given integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int value = 0, digit;
+
+	if (n < 0)
+		_putchar('-');
+
+	while (n)
+	{
+		value = 10 * value + n % 10;
+		n /= 10;
+	}
+
+	while ((value != 0) && (digit != 0))
+	{
+		digit = value % 10;
+		value = value / 10;
+		_putchar('0' + digit);
+	}
+}
