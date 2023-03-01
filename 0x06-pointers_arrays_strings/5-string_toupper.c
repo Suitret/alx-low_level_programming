21f5f06515a0e680d90cdc8270eaccc31d154ff3
Suitret <suitret01@gmail.com>
2023-03-01 02:29:20 -0800

    converts lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: adress of the string
+ * Return: result of change
+ */
+
+char *string_toupper(char *s)
+{
+	int i, n = strlen(tab);
+
+	for (i = 0; i < n; i++)
+		*(s + i) = toupper(*(s + i));
+	return (s);
+}
