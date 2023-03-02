727f5618e2e2aec522c80c835fb4b73c6c259e45
Suitret <suitret01@gmail.com>
2023-03-02 06:46:55 -0800

    rotates alphabetic characters

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * rot13 - rotates alphabetic characters
+ * @str: given string
+ * Return: result string
+ */
+
+char *rot13(char *str)
+{
+	int i, n = strlen(str);
+
+	for (i = 0; i < n; i++)
+	{
+		if (islower(*(s + i)))
+			*(s + i) = ((*(s + i) - 97 + 13) % 26) + 97;
+		else if (isupper(*(s + i)))
+			*(s + i) = ((*(s + i) - 65 + 13) % 26) + 65;
+	}
+	return (str);
+}
