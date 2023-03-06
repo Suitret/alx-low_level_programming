361c7965a1e1fb3635b67b6631237d3f1f0a76d3
Suitret <suitret01@gmail.com>
2023-03-06 11:32:20 -0800

    prints the sum of the two diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,14 @@
  * Return: void
  */
 
-void print_diagsums(int (*a)[], int size)
+void print_diagsums(int *a, int size)
 {
 	int sum1 = 0, sum2 = 0, i;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i][i];
-		sum2 += a[i][size - i - 1];
+		sum1 += a[i * size + i];
+		sum2 += a[i * size  + size - i - 1];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
