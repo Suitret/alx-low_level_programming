0bbf138d17d9b1f12d8a923c02ff210db4dd49b5
Suitret <suitret01@gmail.com>
2023-03-06 10:00:14 -0800

    copies memory area.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of bytes used in memory
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+}
