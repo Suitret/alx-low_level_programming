940b244171575535274ab805d2f0088f67398480
Suitret <suitret01@gmail.com>
2023-03-06 10:16:07 -0800

    searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: pointer on char
+ * @accept: string to identify
+ * Return: s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
