bff3a18c6c15e64edc1168aa4b377bbdaa6af455
Suitret <suitret01@gmail.com>
2023-03-06 10:06:19 -0800

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: pointer on char
+ * @c: character to locate
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
