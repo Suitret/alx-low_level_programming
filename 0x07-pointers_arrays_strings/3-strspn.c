e35c96c1bcf9573b22e47df3c348fff8bf39d6fb
Suitret <suitret01@gmail.com>
2023-03-06 10:11:03 -0800

    gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer on char
+ * @accept: string to identify
+ * Return: lenght
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, n));
+}
