9706356e6a72bb3bc043669654f04290a9a8351f
Suitret <suitret01@gmail.com>
2023-03-08 02:04:17 -0800

    returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: given string
+ * Return: lenght
+ */
+
+int _strlen_recursion(char *s)
+{
+	if(*s)
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
