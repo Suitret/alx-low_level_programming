e1ab14c387576ebb4b6fba60b321c7d9e4fc25ee
Suitret <suitret01@gmail.com>
2023-03-07 20:35:10 -0800

    prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ * @s: string to print
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
