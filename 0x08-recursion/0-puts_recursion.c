a21af3b8e68cd9b705debc1d2baae9cf58b9df0e
Suitret <suitret01@gmail.com>
2023-03-07 21:22:30 -0800

    prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include "main.h"
 
 /**
  * _puts_recursion -  prints a string, followed by a new line
@@ -8,5 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	puts(s);
+	int lenght = strlen(s), i = 0;
+
+	while (i != lenght)
+	{
+		_putchar(s[i]);
+		i++;
+	}
 }
