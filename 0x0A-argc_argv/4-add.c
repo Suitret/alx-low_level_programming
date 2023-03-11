7527aa17d5a1b57b77928a058a9396adf259610e
Suitret <suitret01@gmail.com>
2023-03-11 13:58:56 -0800

    prints the multiplication

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int temp = 0, i, c;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			c = strtol(argv[i], NULL, 10);
+			if (c == 0 && strcmp(argv[i], "0") != 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			temp += c;
+		}
+		printf("%d\n", temp);
+	}
+	return (0);
+}
