28808144f190e9af91048719566449a72a0debcf
Suitret <suitret01@gmail.com>
2023-03-10 18:54:18 -0800

    prints the arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
