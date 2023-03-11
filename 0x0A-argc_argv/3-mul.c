31d4f3e2fe8d678e0b60c74288086d72f5a0d654
Suitret <suitret01@gmail.com>
2023-03-11 10:14:27 -0800

    prints the multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,12 @@ int main(int argc, char *argv[])
 {
 	int temp;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	temp = argv[2] * argv[1];
+	temp = *(*argv + 2) * (*(*argv + 1));
 	printf("%d\n", temp);
 	return (0);
 }
