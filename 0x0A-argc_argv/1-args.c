68c64951235a0a15903a4b38d1b6806726399559
Suitret <suitret01@gmail.com>
2023-03-10 18:40:53 -0800

    prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
