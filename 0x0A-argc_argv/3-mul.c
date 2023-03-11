2a1e55f3e1b676602ed9cef35f770d07bf8f4915
Suitret <suitret01@gmail.com>
2023-03-10 18:58:44 -0800

    prints the multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+		printf("%d\n", argv[0] * argv[1]);
+	else
+		printf("Error\n");
+	return (1);
+}
