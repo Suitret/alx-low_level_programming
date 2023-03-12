5eba23332d4a585e923907590280720575414729
Suitret <suitret01@gmail.com>
2023-03-12 06:34:25 -0700

    adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			c = strtol(argv[i], endptr, 10);
+			c = strtol(argv[i], &endptr, 10);
 			if (c < 0 || (*endptr != '\0'))
 			{
 				printf("Error\n");
