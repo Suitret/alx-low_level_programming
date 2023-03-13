cd3b36b1b5fff2f2c68be6a73f6aa15bb2f71b4f
Suitret <suitret01@gmail.com>
2023-03-13 15:28:59 -0700

    returns a pointer to the concatened string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - returns a pointer to the concatened string
+ * @s1: destination string
+ * @s2: source string
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2;
+	char *p;
+
+	if (s2 == NULL)
+		return (s1);
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	p = (char *)malloc(len1 + len2 + 1);
+
+	if (p == NULL)
+		return (p);
+
+	p = strcat(s1, s2);
+	return (p);
+}
