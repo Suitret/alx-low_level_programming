dfe3696634c65eaf27c4eb7c5a5bff451c56c950
Suitret <suitret01@gmail.com>
2023-03-14 05:50:49 -0700

    concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,12 +17,12 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	if (ac ==  1)
+	if (ac == 1)
 		return (av[0]);
 
 	for (i = 0; i < ac; i++)
 	{
-		len += strlen(av[i]);
+		len += (strlen(av[i]) + 1);
 	}
 
 	p = (char *)malloc(len + 1);
@@ -31,8 +31,11 @@ char *argstostr(int ac, char **av)
 		return (p);
 
 	for (i = 0; i < ac; i++)
-	{	
+	{
 		p = strcat(p, av[i]);
+		len = strlen(av[i]);
+		p[len] = '\n';
+		p[len + 1] = '\0';
 	}
 	return (p);
 }
