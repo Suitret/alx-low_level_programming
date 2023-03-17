a0eb19267412df8f6faa72db56ee2e9f2c21ca01
Suitret <suitret01@gmail.com>
2023-03-16 18:39:26 -0700

    allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,8 @@
+
+
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "main.h"
 
 /**
  * _calloc -  allocates memory for an array, using malloc
