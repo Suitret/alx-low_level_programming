02f36bcb59e166e91628bdc09a71dc69af67ca3c
Suitret <suitret01@gmail.com>
2023-03-20 06:57:06 -0700

    correction

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -9,7 +9,7 @@
  * Return: result of concatenation
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	return (strncat(dest, src, n));
+	return (strncpy(dest, src, n));
 }
