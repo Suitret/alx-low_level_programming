54af27a048537baca704f18099ceb7e2bf5f82ab
Suitret <suitret01@gmail.com>
2023-03-21 01:14:56 -0700

    prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints details of struct dog
+ * @d: pointer
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Age: %0.6f\n", d->age);
+
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+
+}
