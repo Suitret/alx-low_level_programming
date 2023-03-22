1c6ea6050e65448f1947cde00f2d21d92a3dd908
Suitret <suitret01@gmail.com>
2023-03-22 01:03:39 -0700

    creates a dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ptr;
 
 	if (name == NULL || owner == NULL)
-		return (NUL);
+		return (NULL);
 
 	ptr = (dog_t *)malloc(sizeof(dog_t));
 
