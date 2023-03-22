5a32766ffb7cf3fdf7e9cf604ced69dbad2d57b7
Suitret <suitret01@gmail.com>
2023-03-22 03:23:52 -0700

    some files

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -12,3 +12,51 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub - operates substraction
+ * @a: first number
+ * @b: second number
+ * Return: result (int)
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operates multiplication
+ * @a: first number
+ * @b: second number
+ * Return: result (int)
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - operates division
+ * @a: first number
+ * @b: second number
+ * Return: result (int)
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - operates modulo
+ * @a: first number
+ * @b: second number
+ * Return: result (int)
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
