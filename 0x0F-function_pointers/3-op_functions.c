ba804c7ba3894d40666fe02f8cd7e9e319bd1f58
Suitret <suitret01@gmail.com>
2023-03-22 03:17:16 -0700

    some files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - operates addition
+ * @a: first number
+ * @b: second number
+ * Return: result (int)
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
