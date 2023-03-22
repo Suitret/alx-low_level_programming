b4728a285b7511cdc9070f6ff52d28f895e47c11
Suitret <suitret01@gmail.com>
2023-03-22 16:03:39 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - prints opcodes
+ * @f: pointer to function
+ * Return: void
+ */
+
+void print_opcodes(int (*f)(int, char**))
+{
+	unsigned char *opcodes = (unsigned char *)f;
+	int i;
+
+	printf("%.2x", opcodes[0]);
+	for (i = 1; i < sizeof(print_opcodes); i++)
+	{
+		printf(" %.2x", opcodes[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - entry point
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(main);
+	return (0);
+}
+
