4c26d91caa29874670403249ebfe166cdcd68855
Suitret <suitret01@gmail.com>
2023-03-23 02:44:12 -0700

    prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(ap, int);
+		str = va_arg(ap, char *);
 
 		if (str)
 			printf("%s", str);
