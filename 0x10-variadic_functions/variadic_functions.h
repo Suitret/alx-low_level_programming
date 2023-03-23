aedc02bdee266f244c383025dea86e67dad4ddfe
Suitret <suitret01@gmail.com>
2023-03-23 02:11:03 -0700

    adding putchar

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef _FUNCTIONS_
 #define _FUNCTIONS_
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
