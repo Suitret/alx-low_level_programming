5e261f27438eb0b0b81c8b5badad93f29abb3005
Suitret <suitret01@gmail.com>
2023-03-30 14:48:56 -0700

    frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: given list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		if (temp->str)
+			free(temp->str);
+		head = head->next;
+		free(temp);
+    }
+}
