27c715695a0ac4ee104ceb0d35ca6fa372264ec9
Suitret <suitret01@gmail.com>
2023-03-30 15:32:49 -0700

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,21 @@
 section .data
-    hello: db 'Hello, Holberton', 0
+    msg db 'Hello, Holberton', 0Ah, 0
+
 section .text
     global main
+
     extern printf
+
 main:
-    push rbp        ; Save the base pointer
-    mov rbp, rsp    ; Set the base pointer
-    mov edi, hello  ; First argument: the string to print
-    xor eax, eax    ; Set EAX to 0
-    call printf     ; Call printf
-    mov eax, 0      ; Set the return value to 0
-    pop rbp         ; Restore the base pointer
-    ret             ; Return from the function
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [msg]
+    xor rax, rax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
 
