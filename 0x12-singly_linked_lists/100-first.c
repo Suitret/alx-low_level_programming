11567c59fa60df96610e4e4bc716d1ded08149d5
Suitret <suitret01@gmail.com>
2023-03-30 15:14:22 -0700

    executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - executes before main
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
