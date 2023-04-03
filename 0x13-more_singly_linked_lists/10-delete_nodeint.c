4e604e53562894c977a8f0678c5067ed6d56f9e0
Suitret <suitret01@gmail.com>
2023-04-03 07:04:52 -0700

    index not idx

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -35,7 +35,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	ptr1 = *head;
 	temp = 0;
 
-	while (temp != idx - 1)
+	while (temp != index - 1)
 	{
 		temp++;
 		ptr1 = ptr1->next;
