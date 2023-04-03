e3098faf9946d2692a5c967a71f0f417558130e0
Suitret <suitret01@gmail.com>
2023-04-03 07:00:02 -0700

    correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,6 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (temp == 0)
 	{
+		free(ptr1);
 		*head = ptr2;
 		return (1);
 	}
@@ -40,6 +41,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		ptr1 = ptr1->next;
 	}
 
+	free(ptr1->next);
 	ptr1->next = ptr2;
 	return (1);
 }
