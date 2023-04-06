1627a77987efc46377a4320b3a0b58e72f684d38
Suitret <suitret01@gmail.com>
2023-04-06 15:35:02 -0700

    clear a specific bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - sets the bit at index at 0
+ * @n: number
+ * @index: index
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int cpt = 0, temp = 1;
+
+	if (!n)
+		return (-1);
+
+	if (index == 0)
+	{
+		*n = (*n) & 0;
+		return (1);
+	}
+
+	while (cpt != index)
+	{
+		temp *= 2;
+		cpt++;
+	}
+
+	*n = (*n) & (~temp);
+	return (1);
+}
