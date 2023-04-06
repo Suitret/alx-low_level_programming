61ad37040f86455480c011e6833d8a402d9652ed
Suitret <suitret01@gmail.com>
2023-04-06 09:38:37 -0700

    prints number in base 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_binary - prints n in base 2
+ * @n: number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (b > 1)
+		print_binary(b >> 1);
+	_putchar((b & 1) + '0');
+}
