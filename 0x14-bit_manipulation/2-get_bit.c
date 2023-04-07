a0139ac419804149b0ea0a5a2dccb326a7e58c64
Suitret <suitret01@gmail.com>
2023-04-07 02:45:36 -0700

    -1 instead of 0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -33,7 +33,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int idx = max_bits(n), temp = 1;
 
 	if (index > idx - 1)
-		return (0);
+		return (-1);
 
 	if (index == 0)
 		return (n & 1);
