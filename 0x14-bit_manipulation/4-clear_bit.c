712e5d59677520bda29c5740d41eb9f618f73280
Suitret <suitret01@gmail.com>
2023-04-07 03:08:43 -0700

    update of clear_bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -15,6 +15,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (!n)
 		return (-1);
 
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
 	if (index == 0)
 	{
 		*n = (*n) & 0;
