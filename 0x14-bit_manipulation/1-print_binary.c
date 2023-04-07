32236b49d62178cfc2aeebbe623073a2d9794510
Suitret <suitret01@gmail.com>
2023-04-07 02:08:23 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,6 +3,18 @@
 #include "main.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * print_binary - prints n in base 2
  * @n: number
  * Return: void
