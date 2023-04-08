546efd6454d94db6c9250551b4dd0748e037c111
Suitret <suitret01@gmail.com>
2023-04-08 12:47:38 -0700

    read and print text from a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads the content of filename
+ * @filename: file to read
+ * @letters: numbers of chars to print
+ * Return: number of chars printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	size_t count = 0;
+	char c;
+
+	if (!filename)
+		return (0);
+
+	fp = fopen(filename, "r");
+
+	if (!fp)
+		return (0);
+
+	do {
+		c = fgetc(fp);
+		_putchar(c);
+		count++;
+	} while (count != letters && c != EOF);
+
+	fclose(fp);
+	return (count);
+}
