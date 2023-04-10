13071bb8fe6fd6b456fe4450fc249a040d23485e
Suitret <suitret01@gmail.com>
2023-04-10 16:48:21 -0700

    creatin

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t rp;
 	int fd;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
