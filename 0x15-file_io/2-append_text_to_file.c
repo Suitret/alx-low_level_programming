71dad10af879e6be358d31cf799732e03bf21cf8
Suitret <suitret01@gmail.com>
2023-04-11 06:34:56 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,6 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+		return (1);
 
 	fd = open(filename, O_APPEND | O_WRONLY);
 
