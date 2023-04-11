b48edaaaf9cb5ff5cc357ab10a73309e00c133dc
Suitret <suitret01@gmail.com>
2023-04-11 06:09:48 -0700

    O_EXCL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_EXCL | O_WRONLY, 0600);
 
 	if (fd == -1)
 		return (-1);
