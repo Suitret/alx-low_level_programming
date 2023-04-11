c4da0563e4ad52094aa4961c0c36206735a49711
Suitret <suitret01@gmail.com>
2023-04-10 17:12:13 -0700

    creation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t rp;
 	int fd;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
