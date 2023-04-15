e96387c839edf8065440946c8db89eb7f316fd0a
Suitret <suitret01@gmail.com>
2023-04-14 19:58:07 -0700

    file empty

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	
+	return (0);
+}
