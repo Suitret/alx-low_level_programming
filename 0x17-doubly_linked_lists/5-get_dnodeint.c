73125563ddd97907e674443b6d6d38ec8c072ca5
Suitret <suitret01@gmail.com>
2023-05-18 11:08:33 -0700

    5. Get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - adds a new node at the end of a listint_t list
+ * @head:  pointer to the head of list
+ * @index: number
+ * Return: adress
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int temp = 0;
+
+	while (temp != index && head)
+	{
+		temp++;
+		head = head->next;
+	}
+
+	if (temp == index)
+		return (head);
+	else
+		return (NULL);
+}
