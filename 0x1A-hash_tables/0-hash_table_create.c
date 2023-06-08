9c21ebb84c3ffbf6c0d1bb6e05b2b359a7fc5fa9
Suitret <suitret01@gmail.com>
2023-06-08 03:40:19 -0700

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: the size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+
+	if (!table->array)
+		return (NULL);
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
