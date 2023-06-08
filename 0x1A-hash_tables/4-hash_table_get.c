6dcd2c9081cc356ea8bd1c493cb0f7f1d8cbd7e4
Suitret <suitret01@gmail.com>
2023-06-08 08:20:40 -0700

    4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves an element from the hash table.
+ * @key:  the key
+ * @ht: the hash table
+ * Return: the value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *item;
+
+	if (!ht || !ht->array)
+		return (NULL);
+
+	index = key_index(key, ht->size);
+	item = ht->array[index];
+
+	if (item)
+	{
+		while (item)
+		{
+			if (strcmp(item->key, key) == 0)
+				return (item->value);
+			item = item->next;
+		}
+	}
+	return (NULL);
+}
