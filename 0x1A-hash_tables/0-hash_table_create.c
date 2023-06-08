0cc5b4618ea3feafdacf381523981d7769a05bea
Suitret <suitret01@gmail.com>
2023-06-08 03:41:54 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = calloc(table->size, sizeof(hash_node_t *));
 
 	if (!table->array)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
