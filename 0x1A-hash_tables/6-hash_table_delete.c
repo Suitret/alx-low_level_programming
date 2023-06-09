d0b444a5bd8f6d01f9335ab727a0c72927430528
Suitret <suitret01@gmail.com>
2023-06-09 03:24:07 -0700

    6. >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes the hash table.
+ * @ht: the hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *item;
+	unsigned long int i;
+
+	if (ht && ht->array && ht->size)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			item = ht->array[i];
+			if (item)
+			{
+				free(item->key);
+				free(item->value);
+				free(item);
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
